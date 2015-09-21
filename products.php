46498bddfb2b5be14c4a14ec4225bfd68c04e660
achhabra95 <aishwarya.chhabra95@gmail.com>
2015-09-21 00:15:05 -0500

    Create products.php

---
+++ products.php
@@ -0,0 +1,21 @@
+<?php
+//list of all the products in the store
+$_SESSION['phones']=array( 'Apple iPhone 6'=> array ( 'price'=> 900 , 'quantity' => 0),
+						   'Google Nexus 6'=> array ( 'price'=> 500 , 'quantity' => 0),
+                           'Motorola Moto X'=> array ( 'price'=> 400 , 'quantity' => 0),
+                           'Samsung Galaxy S6'=> array ( 'price'=> 900 , 'quantity' => 0),
+                           );
+$_SESSION['tablets']=array( 'Apple iPad Air 2'=> array ( 'price'=> 950 , 'quantity' => 0),
+                            'Apple iPad mini 3'=> array ( 'price'=> 750 , 'quantity' => 0),
+						    'Google Nexus 9'=> array ( 'price'=> 400 , 'quantity' => 0),
+                            'Microsoft Surface Pro3'=> array ( 'price'=> 700 , 'quantity' => 0),
+                           );
+						  
+$_SESSION['wearables']=array( 'Apple Watch'=> array ( 'price'=> 400 , 'quantity' => 0),
+                            'Motorola Moto 360'=> array ( 'price'=> 250 , 'quantity' => 0),
+						    'Pebble Steel' =>array ( 'price'=> 100 , 'quantity' => 0),
+                           );						  
+						   
+							
+
+?>
